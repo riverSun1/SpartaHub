70a57daa195c0e90ba67e69f7924843692a8e500
LeeHareum <g029843507@gmail.com>
2024-06-04 06:20:00 +0900

    add:MyBoard/update:페이지네이션/script:react-paginate

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,17 +1,10 @@
-import styled from "styled-components";
-import "../styles/designToken.css";
-
-const Container = styled.div`
-  background-color: var(--red-70);
-  height: 100vh;
-  width: 30vw;
-`;
+import MyBoard from "../components/MyBoard/MyBoard";
 
 const MyPage = () => {
   return (
-    <Container>
-      <div>MyPage</div>
-    </Container>
+    <div>
+      <MyBoard />
+    </div>
   );
 };
 
