593e0e883c5a28063e48f31251427a25dcca5aa5
LeeHareum <g029843507@gmail.com>
2024-06-03 21:33:19 +0900

    add:prettierrc/style:designToken

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import "../styles/designToken.css";
+
+const Container = styled.div`
+  background-color: var(--red-70);
+  height: 100vh;
+  width: 30vw;
+`;
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <Container>
+      <div>MyPage</div>
+    </Container>
+  );
 };
 
 export default MyPage;
