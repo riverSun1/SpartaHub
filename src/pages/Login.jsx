0f323e6256c4c81ca544ace148a05976b454fa2d
Elephant24456 <rlaxortn0984@naver.com>
2024-06-04 10:46:16 +0900

    feat: 로그인 기능 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,53 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!email.trim()) {
+      alert("이메일을 입력해 주세요.");
+      return;
+    }
+    
+    if (!password.trim()) {
+      alert("비밀번호를 입력해 주세요.");
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">이메일:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">비밀번호:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
 export default Login;
