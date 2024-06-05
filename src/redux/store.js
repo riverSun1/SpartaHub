239e959e048665a9a345da87450a4628c1d324e4
Elephant24456 <rlaxortn0984@naver.com>
2024-06-05 19:36:59 +0900

    feat: 로그인 기능 구현, styled: css 구현,add: store.js, userSlice.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsSlice from "./slices/postsSlice";
+import userSlice from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
+    user: userSlice,
     posts: postsSlice
   }
 });
